bdf9c6bdcd8f208e44014695a65976d295659d5f
agostinopique <agostino.pique@gmail.com>
2022-10-03 17:40:41 +0200

    Completati nuovi esercizi, da 1 a numero 4 incluso;

---
+++ Controllers/EserciziController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class EserciziController : Controller
+    {
+        public IActionResult Es1()
+        {
+            return View();
+        }
+
+        public IActionResult Es2()
+        {
+            return View();
+        }
+        public IActionResult Es3()
+        {
+            return View();
+        }
+        public IActionResult Es4()
+        {
+            return View();
+        }
+    }
+}
